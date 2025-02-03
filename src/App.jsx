83b7d34d77b3849a5e004516ed82a7e73c421408
OmSharma167 <op809034@gmail.com>
2025-02-03 21:04:31 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import OutletSettings from "./pages/OutletSettings";
 import OutletInfo from "./pages/OutletInfo";
 import Help from "./pages/Help";
 import AddItemForm from "./components/AddItemForm";
-import PopularLocations from "../practicess/PopularLocations";
+// import PopularLocations from "../practicess/PopularLocations";
 import TaxesAndCharges from "./components/TaxesAndCharges/TaxAndCharges";
 
 
@@ -30,7 +30,7 @@ export default function App() {
         <Route path="/outlet-settings" element={<OutletSettings />} />
         <Route path="/outlet-info" element={<OutletInfo />} />
         <Route path="/help" element={<Help />} />
-        <Route path="/PopularLocations" element={<PopularLocations />} />
+        {/* <Route path="/PopularLocations" element={<PopularLocations />} /> */}
         <Route path="/taxes-charges" element={<TaxesAndCharges />} />
         {/* If you need more pages, add them here */}
       </Route>
